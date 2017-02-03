59051881da0270df3f74e298e21c40c6d1ca6b5c
Federico Zucca <zucca.federico@gmail.com>
2017-02-03 11:04:45 +0000

    map wrapper setted up

---
+++ client/src/models/mapWrapper.js
@@ -0,0 +1,28 @@
+var MapWrapper = function (container, coords, zoom) {
+  this.googleMap = new google.maps.Map(container, {
+    center: coords,
+    zoom: zoom
+  })
+}
+
+MapWrapper.prototype ={
+  addMarker: function(coords, contentInfo){
+    var infoWindow = new google.maps.infoWindow({
+      content: contentInfo
+    });
+    var maker = new google.maps.Marker({
+      position: coords,
+      map: this.googleMap,
+      animation: google.maps.Animation.DROP
+    });
+    marker.addListener('click', function (){
+      infoWindow.open(this.googleMap, marker);
+    });
+  },
+  centreMap: function (button, coords, zoom){
+    button.onclick = function () {
+      this.googleMap.setCenter(coords);
+      this.googleMap.setZoom(zoom);
+    }.bind(this);
+  }
+}
