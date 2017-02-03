e02278ede6f9857e8563f03ce1ced1993a628336
Elgrogz <gregorgilchrist@gmail.com>
2017-02-03 11:56:43 +0000

    merge conflict resolved

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,19 +1,10 @@
 config = {
-<<<<<<< HEAD
   entry: "./src/app.js",
   output: {
     filename: "bundle.js",
     path: "./build"
   },
   devtool: 'source-map'
-=======
- entry: "./src/app.js",
- output: {
-   filename: "bundle.js",
-   path: "./build"
- },
- devtool: 'source-map'
->>>>>>> develop
 }
 
 module.exports = config;
